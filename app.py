8a6c11a3986bca58f02e47e4cdc183354c525f60
Alejandro Llamas <allamass95@gmail.com>
2019-10-16 21:18:59 -0500

    files

--- app.py
+++ app.py
@@ -1,6 +1,40 @@
 import pandas as pd
 from flask import (Flask,render_template,jsonify)
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+
+app = Flask(__name__)
 
 
+@app.route("/api/democrat")
+def democrat():
+    connection_string='postgres:Alevaporo58@localhost:5432/project_2'
+    engine=create_engine(f'postgresql://{connection_string}')
+    conn=engine.connect()
+    sql_data=f'select * from finalized_dem'
+    dem_df=pd.read_sql(sql_data,conn)
+    conn.close()
+    return dem_df.to_json(orient='records')
 
+@app.route("/api/republican")
+def republican():
+    connection_string='postgres:Alevaporo58@localhost:5432/project_2'
+    engine=create_engine(f'postgresql://{connection_string}')
+    conn=engine.connect()
+    sql_data=f'select * from finalized_rep'
+    rep_df=pd.read_sql(sql_data,conn)
+    conn.close()
+    return rep_df.to_json(orient='records')
+
+@app.route("/api/split")
+def split():
+    connection_string='postgres:Alevaporo58@localhost:5432/project_2'
+    engine=create_engine(f'postgresql://{connection_string}')
+    conn=engine.connect()
+    sql_data=f'select * from finalized_split'
+    split_df=pd.read_sql(sql_data,conn)
+    conn.close()
+    return split.to_json(orient='records')
+
+if __name__ =='__main__':
+    app.run()
---
+++ static/js/rep_data.js
@@ -0,0 +1,80 @@
+rep_data=
+[
+    {
+    "Median Household Income": 49243.1967686476,
+    "Per Capita Income": 25845.3577025626,
+    "Poverty Rate": 14.6401555639,
+    "Unemployment Rate": 4.3667685778,
+    "Median Cost of Rent": 783.4352422341,
+    "Black Poverty Rate": 26.5639632836,
+    "Hispanic Poverty Rate": 26.0873378153,
+    "White Poverty Rate": 11.9293894334,
+    "year": "2011"
+    },
+    {
+    "Median Household Income": 49419.6217569651,
+    "Per Capita Income": 25956.4801504539,
+    "Poverty Rate": 15.2233929411,
+    "Unemployment Rate": 4.6390739525,
+    "Median Cost of Rent": 799.6135689913,
+    "Black Poverty Rate": 27.2423751693,
+    "Hispanic Poverty Rate": 26.9814356546,
+    "White Poverty Rate": 12.4935314488,
+    "year": "2012"
+    },
+    {
+    "Median Household Income": 49471.731067314,
+    "Per Capita Income": 26040.3617962469,
+    "Poverty Rate": 15.699211404,
+    "Unemployment Rate": 4.8000330074,
+    "Median Cost of Rent": 812.9950818498,
+    "Black Poverty Rate": 27.8331806375,
+    "Hispanic Poverty Rate": 27.5284490436,
+    "White Poverty Rate": 12.9241499916,
+    "year": "2013"
+    },
+    {
+    "Median Household Income": 49965.0431718543,
+    "Per Capita Income": 26435.440592452,
+    "Poverty Rate": 15.8666845151,
+    "Unemployment Rate": 4.4777453333,
+    "Median Cost of Rent": 827.3090465976,
+    "Black Poverty Rate": 27.8372256305,
+    "Hispanic Poverty Rate": 27.4051222113,
+    "White Poverty Rate": 13.1080761746,
+    "year": "2014"
+    },
+    {
+    "Median Household Income": 50437.4844123104,
+    "Per Capita Income": 26821.6552900855,
+    "Poverty Rate": 15.6847988082,
+    "Unemployment Rate": 3.9935581304,
+    "Median Cost of Rent": 834.4447713424,
+    "Black Poverty Rate": 27.4332374566,
+    "Hispanic Poverty Rate": 26.7827386908,
+    "White Poverty Rate": 12.9937996512,
+    "year": "2015"
+    },
+    {
+    "Median Household Income": 51805.0040896422,
+    "Per Capita Income": 27657.9040114343,
+    "Poverty Rate": 15.2690525017,
+    "Unemployment Rate": 3.5436887719,
+    "Median Cost of Rent": 855.6239665721,
+    "Black Poverty Rate": 26.5302067696,
+    "Hispanic Poverty Rate": 25.6681822418,
+    "White Poverty Rate": 12.6924957717,
+    "year": "2016"
+    },
+    {
+    "Median Household Income": 56026.2262586018,
+    "Per Capita Income": 30271.1468145836,
+    "Poverty Rate": 15.1265376281,
+    "Unemployment Rate": 3.3391046666,
+    "Median Cost of Rent": 960.3305267891,
+    "Black Poverty Rate": 24.6486458553,
+    "Hispanic Poverty Rate": 23.0802990527,
+    "White Poverty Rate": 12.5755137569,
+    "year": "2017"
+    }
+    ]
